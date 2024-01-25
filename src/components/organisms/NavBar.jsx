e53062516b7536ebd347b2232cf62966688c3593
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-01-25 05:11:37 -0600

    [ADD] navbar has been create

---
+++ src/components/organisms/NavBar.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+
+const NavBar = () => {
+    const { user, logout } = useAuth();
+
+    return (
+        <nav className="bg-gray-800 p-4 text-white">
+            <ul className="flex space-x-4">
+                <li><Link to="/">Inicio</Link></li>
+                {!user && (
+                    <>
+                        <li><Link to="/login">Iniciar Sesión</Link></li>
+                        <li><Link to="/register">Registrarse</Link></li>
+                    </>
+                )}
+                {user && (
+                    <li>
+                        <button onClick={logout}>Cerrar Sesión</button>
+                        <Link to="/profile">Perfil</Link>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
