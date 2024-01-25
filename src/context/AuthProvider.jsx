aae8d4271db9adc50cf01903631d14918453cfe7
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-01-25 12:40:15 -0600

    [ADD] AuthProvider

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const login = (token) => {
+        localStorage.setItem('token', token);
+        setUser({ token });
+    };
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setUser(null);
+    };
+
+    const register = (token) => {
+        localStorage.setItem('token', token);
+        setUser({ token });
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout, register }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
